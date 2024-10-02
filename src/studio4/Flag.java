3a071d574a4cf9e2f5eab318d6eff64e62f80812
davidduong <davidduong@david-jr-iii.dhcp.wustl.edu>
2024-10-02 14:15:39 -0500

    Here's the commit and push!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,22 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(5,70,60);
+		StdDraw.filledRectangle(0.5, 0.55, 0.35, 0.25);
+		StdDraw.setPenColor(0,40,80);
+		StdDraw.filledRectangle(0.5, 0.55, 0.34, 0.24);
+		StdDraw.setPenColor(50,50,0);
+		StdDraw.filledRectangle(.5, 0.5, .025, .15);
+		
+		double[] xTriangleOne = {0.35,0.65,0.50};
+		double[] yTriangleOne = {0.5,0.5,0.70};
+		double[] xTriangleTwo = {0.3,0.70,0.50};
+		double[] yTriangleTwo = {0.40,0.40,0.60};
+		
+		StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+		StdDraw.filledCircle(.5, 0.7, 0.04);
+		StdDraw.setPenColor(5,80,5);
+		StdDraw.filledPolygon(xTriangleOne, yTriangleOne);
+		StdDraw.filledPolygon(xTriangleTwo, yTriangleTwo);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,48 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
-	}
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double xCord = in.nextDouble();
+		double yCord = in.nextDouble();
+		double xThick = in.nextDouble();
+		double yThick = in.nextDouble();
+		//double xVert = in.nextDouble();
+		//double yVert = in.nextDouble();
+		
+		if (shape=="rectangle") {
+		StdDraw.setPenColor(red,green,blue);
+			if (filled==true) {
+			StdDraw.filledRectangle(xCord, yCord, xThick, yThick);
+			}
+			else {
+			StdDraw.rectangle(xCord, yCord, xThick, yThick);
+			}
+		}
+		
+		if (shape=="ellipse") {
+			StdDraw.setPenColor(red,green,blue);
+			if (filled==true) {
+				StdDraw.filledEllipse(xCord, yCord, xThick, yThick);
+			}
+			else {
+				StdDraw.ellipse(xCord, yCord, xThick, yThick);
+				}
+			}
+		
+		if (shape=="triangle") {
+			StdDraw.setPenColor(red,green,blue);
+			//double[] x = {xCord,xThick,xVert};
+			//double[] y = {yCord,yThick,yVert};
+			if (filled==true) {
+				//StdDraw.filledPolygon(x, y);
+			}
+			else {
+				//StdDraw.polygon(x, y);
+				}
+			}
+	}	
 }
